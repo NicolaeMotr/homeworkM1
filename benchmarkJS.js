4787bdd5d93f97298a3358fcabb1c7a1cd506eac
Nicolae Motricala <motricala.nicola@gmail.com>
2020-10-12 12:08:16 +0200

    post lezione 12/10/2020

--- benchmarkJS.js
+++ benchmarkJS.js
@@ -1,6 +1,12 @@
 let x = "John";
 let y = "Doe";
 
+const print = function{
+  let x = "John"
+  let y = "Doe"
+  console.log(x + ' <>' + y)
+}
+
 function compareNames(name1, name2) {
   if (name1 === name2) {
     console.log(name1, "==", name2);
@@ -36,7 +42,18 @@ let myArray = [
 ];
 console.log(myArray);
 
+// a lezione:
+const createArray = function() {
+  let arrayLezione = []
+  for(let x=0; x<10; x++) {
+    array.push('element n' + x)
+  }
+  console.log(arrayLezione)
+}
+
 let secondArray = [];
+
+//
 
 function getRandomNumbers() {
   return Math.floor(Math.random() * 1000) + 1;
@@ -68,3 +85,27 @@ for (let index = 0; index < 10; index++) {
 }
 
 console.log("ARRAY OF ARRAYS", thirdArray);
+
+
+// ex. 29
+
+const longestArray = function(arr1, arr2) {
+  return arr1.lenght > arr2.lenght? arr1 : arr2;
+}
+
+// ex. 30
+
+const higherValueArray = function(arr1, arr2) {
+  let sum1 = 0
+  let sum2 = 0
+  for(let x=0; x<arr1.lenght; x++) {
+    sum1 = sum1 + arr1[x]
+    // versione più corta sum1 += arr1[x]
+  }
+  for(let y=0; y<arr2.lenght; y++) {
+    sum2= sum2 + arr2[y]
+    // sum2 += arr2[y]
+  }
+  // sum1 and sum2 are the actual sum of the values
+  return sum1 > sum2 ? sum1 : sum2 
+}
